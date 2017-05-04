7cf047bf27a34c54d0a7754495b4e765cf3dce89
Elliot Kirk <elliot.s.kirk@gmail.com>
2017-05-04 15:13:48 -0700

    More files

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+//$(document).ready(function () {
+//
+//    var menu = $('.menu');
+//    var origOffsetY = menu.offset().top;
+//
+//    function scroll() {
+//        if ($(window).scrollTop() >= origOffsetY) {
+//            $('.menu').addClass('sticky');
+//        } else {
+//            $('.menu').removeClass('sticky');
+//        }
+//    }
+//    document.onscroll = scroll;
+//});
