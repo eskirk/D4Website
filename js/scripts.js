446f202adef4c994ee88d8c003f9f834d0af9608
Elliot Kirk <elliot.s.kirk@gmail.com>
2017-05-16 11:08:45 -0700

    Cool clickable contentz!

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,45 @@
-$(document).ready(function () {
-    
-    $('.camera_wrap').camera(); //the basic method
+/** Used for toggling the descriptions on the Gear page **/
 
-    $('.camera_wrap').camera({
-	   thumbnails: true
-    });
-    
-});
+function toggle1(divId) {
+    $("#" + divId).toggleClass("hidden", false);
+    $("#" + "choice1").toggleClass("selected", true);
+    $("#" + "innov2").toggleClass("hidden", true);
+    $("#" + "innov3").toggleClass("hidden", true);
+    $("#" + "innov4").toggleClass("hidden", true);
+    $("#" + "choice2").toggleClass("selected", false);
+    $("#" + "choice3").toggleClass("selected", false);
+    $("#" + "choice4").toggleClass("selected", false);
+}
 
+function toggle2(divId) {
+    $("#" + divId).toggleClass("hidden", false);
+    $("#" + "choice2").toggleClass("selected", true);
+    $("#" + "innov1").toggleClass("hidden", true);
+    $("#" + "innov3").toggleClass("hidden", true);
+    $("#" + "innov4").toggleClass("hidden", true);
+    $("#" + "choice1").toggleClass("selected", false);
+    $("#" + "choice3").toggleClass("selected", false);
+    $("#" + "choice4").toggleClass("selected", false);
+}
+
+function toggle3(divId) {
+    $("#" + divId).toggleClass("hidden", false);
+    $("#" + "choice3").toggleClass("selected", true);
+    $("#" + "innov1").toggleClass("hidden", true);
+    $("#" + "innov2").toggleClass("hidden", true);
+    $("#" + "innov4").toggleClass("hidden", true);
+    $("#" + "choice1").toggleClass("selected", false);
+    $("#" + "choice2").toggleClass("selected", false);
+    $("#" + "choice4").toggleClass("selected", false);
+}
+
+function toggle4(divId) {
+    $("#" + divId).toggleClass("hidden", false);
+    $("#" + "choice4").toggleClass("selected", true);
+    $("#" + "innov1").toggleClass("hidden", true);
+    $("#" + "innov2").toggleClass("hidden", true);
+    $("#" + "innov3").toggleClass("hidden", true);
+    $("#" + "choice1").toggleClass("selected", false);
+    $("#" + "choice2").toggleClass("selected", false);
+    $("#" + "choice3").toggleClass("selected", false);
+}
