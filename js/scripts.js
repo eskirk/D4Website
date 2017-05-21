1517fe7971bc5d5e1d922c4e8a50e35c2384395f
Elliot Kirk <elliot.s.kirk@gmail.com>
2017-05-21 11:37:58 -0700

    Image galleries

--- js/scripts.js
+++ js/scripts.js
@@ -63,3 +63,21 @@ $(document).ready(function(){
         e.preventDefault();
     }, false);
 });
+
+$(document).ready(function(){
+    var th = document.getElementById('thumbnails2');
+    
+    th.addEventListener('click', function(e) {
+        var t = e.target, new_src = t.parentNode.href, 
+            large = document.getElementById('large2'),
+            cl = large.classList,
+            lgwrap = document.getElementById('lg-wrap2');
+        lgwrap.style.backgroundImage = 'url(' +large.src + ')';
+        if(cl) cl.add('hideme');
+        window.setTimeout(function(){
+            large.src = new_src;
+            if(cl) cl.remove('hideme');
+        }, 50);
+        e.preventDefault();
+    }, false);
+});
